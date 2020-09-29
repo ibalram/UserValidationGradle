9c69bd2dbb768b83bd1fe14a551087dc3c7e5dcf
ibalram <balramsinghrathore3@gmail.com>
2020-09-30 01:15:06 +0530

    Added: JUnit Test to validate multiple email

---
+++ src/test/java/com/cg/EmailValidatorTest.java
@@ -0,0 +1,61 @@
+package com.cg;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailValidatorTest {
+	private UserRegistration userRegistration;
+	private String email;
+	private boolean expected;
+	
+	public EmailValidatorTest(String email, boolean expected) {
+		this.email = email;
+		this.expected = expected;
+	}
+	
+	@Before
+	public void initialize() {
+		userRegistration = new UserRegistration(); 
+	}
+	
+	@Parameterized.Parameters
+	public static Collection input() {
+		return Arrays.asList(new Object[][] {
+			{"abc@yahoo.com",true},
+			{"abc-100@yahoo.com",true},
+			{"abc.100@yahoo.com",true},
+			{"abc111@abc.com",true},
+			{"abc-100@abc.net",true},
+			{"abc.100@abc.com.au",true},
+			{"abc@1.com",true},
+			{"abc@gmail.com.com",true},
+			{"abc+100@gmail.com",true},
+			{"abc",false},
+			{"abc@.com.my",false},
+			{"abc123@gmail.a",false},
+			{"abc123@.com",false},
+			{"abc123@.com.com",false},
+			{".abc@abc.com",false},
+			{"abc()*@gmail.com",false},
+			{"abc@%*.com",false},
+			{"abc..2002@gmail.com",false},
+			{"abc.@gmail.com",false},
+			{"abc@abc@gmail.com",false},
+			{"abc@gmail.com.1a",false},
+			{"abc@gmail.com.aa.au",false}
+		});
+	}
+	
+	@Test
+	public void testEmailValidatorTest() {
+		assertEquals(expected, userRegistration.emailValidator(email));
+	}
+	
+
+}
